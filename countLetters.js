f2a0549f2bc04851b0fabbe2cb6fcb6c688d2d62
alfficcadenti <alfredoficcadenti@gmail.com>
2019-01-11 20:21:35 +0000

    initial commit

--- countLetters.js
+++ countLetters.js
@@ -1,28 +1,21 @@
 function countLetters(string) {
 // create a object as result
 var letterCount = {};
+string = string.replace(/\s/g, '');
+
 //for loop the input string
   // increase the key in the object result for the char
 
   for (i = 0; i < string.length; i++) {
     var key = string[i]
     if (letterCount[key]==undefined) {
-            letterCount[key] = 1
+        letterCount[key] = 1
       }
     else {
-      letterCount[key] = letterCount[key]+1
+      letterCount[key] += 1
     }
-    //console.log(letter)
-    //var letterCount = { "letter": 1 };
-
-     //if (letterCount[letter]==undefined) {
-
-     //}
-
-    // var letterCount = {string[i]:
   }
-  //console.log(letterCount)
-         console.log(letterCount)
+  console.log(letterCount)
 
 }
 
